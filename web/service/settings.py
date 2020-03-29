ff30ef31a9303900161969d0521d234cec2316f2
Voleg <ffrooty@gmail.com>
2020-03-29 23:16:17 +0300

    Added CORS config for development.

--- web/service/settings.py
+++ web/service/settings.py
@@ -42,10 +42,14 @@ INSTALLED_APPS = [
     'algos'
 ]
 
+if DEBUG:
+    INSTALLED_APPS += ['corsheaders']
+    CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     # 'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
