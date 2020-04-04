d3a738abba5a1b1f5f1ac5d84c6a07ce6a856820
Voleg <ffrooty@gmail.com>
2020-04-05 02:59:18 +0300

    Added factorial function on for loop.

--- algos/algos/implementations/factorial.py
+++ algos/algos/implementations/factorial.py
@@ -4,7 +4,27 @@ log = logging.getLogger(__name__)
 
 
 def factorial_naive(n: int) -> int:
+    """
+    CPython: 18.5 µs ± 250 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    """
     if n <= 2:
         return n
-
+    
     return n * factorial_naive(n - 1)
+
+
+def factorial_for_loop(n: int, acc: int = 1) -> int:
+    """
+    CPython 10.6 µs ± 104 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    PyPy3:  3.33 µs ± 257 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+    """
+    if n <= 2:
+        return n
+    for i in range(n + 1):
+        if i == 0:
+            continue
+        acc = acc * i
+    return acc
+
+def factorial(n):
+    pass
