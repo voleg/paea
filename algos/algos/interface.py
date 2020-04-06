cf86ceb80a131b09541179d2f342ac73dbc4593f
Voleg <ffrooty@gmail.com>
2020-04-06 17:43:21 +0300

    Switched to Ackerman function with cache implementation.

--- algos/algos/interface.py
+++ algos/algos/interface.py
@@ -1,6 +1,7 @@
 from algos.implementations import factorial_naive
 from algos.implementations import fibonacci_naive
 from algos.implementations import ackerman_naive
+from algos.implementations.ackerman import ackermann_memo_func
 
 
 class AlgoTools(object):
@@ -9,7 +10,7 @@ class AlgoTools(object):
         self.algorithms ={
             'factorial': factorial_naive,
             'fibonacci': fibonacci_naive,
-            'ackermann': ackerman_naive,
+            'ackermann': ackerman_memo_func,
         }
 
     def get_function_by_name(self, name):
