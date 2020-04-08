2987edf990c1d6a46e646da3dbba58522407eadd
Voleg <ffrooty@gmail.com>
2020-04-09 02:41:21 +0300

    Added more Fibonacci implementations.

--- algos/algos/implementations/fibonacci.py
+++ algos/algos/implementations/fibonacci.py
@@ -1,4 +1,6 @@
+from math import sqrt, pow
 import logging
+
 
 log = logging.getLogger(__name__)
 
@@ -8,3 +10,25 @@ def fibonacci_naive(n: int) -> int:
         return n
 
     return fibonacci_naive(n - 1) + fibonacci_naive(n - 2)
+
+
+def fibonacci_Binet_form(n: int) -> int:
+    # TODO: drop out this ( at n=71 produses incorrect values)
+    return int(
+        1/sqrt(5) * ( pow((1+sqrt(5))/2, n) - pow((1-sqrt(5))/2, n))
+    )
+
+
+def fibonacci_memo(n: int, cache: dict = {}) -> int:
+    if n <= 1:
+        return n
+
+    if not cache.get(n-1):
+        cache[n-1] = fibonacci_memo(n - 1, cache=cache)
+
+    if not cache.get(n-2):
+        cache[n-2] = fibonacci_memo(n - 2, cache=cache)
+
+    return cache[n - 1] + cache[n - 2]
+
+
