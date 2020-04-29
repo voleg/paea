fab7459857d8b59ed82fa0da7ae5274cd3cf09c8
Voleg <ffrooty@gmail.com>
2020-04-29 14:58:54 +0300

    Disabled noisy logs from UWSGI.

--- web/deploy/start_web_server.sh
+++ web/deploy/start_web_server.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-uwsgi --ini /code/deploy/uwsgi.ini --enable-threads
+sleep 25
+python manage.py index --reset || true
+uwsgi --ini /code/deploy/uwsgi.ini --enable-threads --disable-logging
