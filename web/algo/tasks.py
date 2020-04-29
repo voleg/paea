ee61f059e7f3cfbd44edc5b61401143152269e1b
Voleg <ffrooty@gmail.com>
2020-04-29 15:46:23 +0300

    Result saving task logging update.

--- web/algo/tasks.py
+++ web/algo/tasks.py
@@ -8,15 +8,15 @@ log = get_task_logger(__name__)
 
 @task(name='store_calculation_result')
 def store_calculation_result(**kwargs):
-    log.info(f'Calculation results recieved {kwargs}')
+    log.debug(f'Calculation results received: {kwargs}')
     calc_id = kwargs.get('id')
     status_message = kwargs.get('status')
     result = kwargs.get('result')
     doc = Calculation.get(id=calc_id)
-    doc.meta.version=None
+    doc.meta.version = None
     doc.update(
         status=status_message,
-        result = result,
+        result=result,
         retry_on_conflict=10
     )
-    log.info('Calculation #{} result saved'.format(calc_id))
+    log.info(f'Calculation ID:{calc_id}, STATUS: {status_message} result saved')
